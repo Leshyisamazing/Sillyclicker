50650441f0a6b4c480b7afd2f4b051201b7e904b
Leshyisamazing <152522209+Leshyisamazing@users.noreply.github.com>
2025-01-13 12:40:03 -0500

    Disappearing buttons added

--- src/main.js
+++ src/main.js
@@ -2,18 +2,30 @@ const tickRate = 1000 / 30;
 let score = 0;
 
 let confetti = new Building("Confetti", 0.1, 15, "buyConfetti");
+let partyhat = new Building("Partyhat", 1, 100,"buyConfetti");
+let presents = new Building("Presents", 2, 200, "buyPresents" );
 
 function incScore() {
     score += confetti.cps;
+    score += partyhat.cps;
+    score += presents.cps;
 }
 
-function scorePlusPlus(){
+function scorePlusPlus() {
     score++;
 }
 
+function updateButtons(){
+    confetti.buttonState();
+    partyhat.buttonState();
+    presents.buttonState();
+}
+
+
 function updatePage() {
     incScore();
-    document.getElementById("score").innerHTML = "$"+ Math.floor(score).toLocaleString();
+    document.getElementById("score").innerHTML = 
+    '$' + Math.floor(score).toLocaleString();
 }
 
 setInterval(updatePage, tickRate);
